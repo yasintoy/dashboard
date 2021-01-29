20e0ac83de569577de45a0ab47c7ff2229766f98
yasintoy <yasin.toy@yandex.com>
2021-01-29 04:19:20 +0300

    Filter options moved into Constants.js

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,15 @@
 const Constants = {
   LIGHT_MODE: 'light',
   DARK_MODE: 'dark',
+  FILTERS: [
+    'saved',
+    'pending',
+    'active',
+    'paused',
+    'expired',
+    'declined',
+    'terminated',
+  ],
 };
 
 export default Constants;
